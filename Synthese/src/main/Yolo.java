c06f56ddc59405ed8071d2a248fbb2e7ef0ffe51
lbombel <zurek.nicolas.2.0@gmail.com>
2015-01-19 00:37:38 +0100

    first commit

---
+++ Synthese/src/main/Yolo.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Yolo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("===================== Yolo =====================");
+	}
+
+}
