0b4d3a30d592dc4b7852d2f49b77cdc38da54425
zurekn <zurek.nicolas@gmail.com>
2015-01-21 13:03:09 +0100

    Add slick

---
+++ Synthese/src/main/WindowGame.java
@@ -0,0 +1,40 @@
+package main;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+
+public class WindowGame extends BasicGame {
+	
+	 private GameContainer container;
+
+	    public static void main(String[] args) throws SlickException {
+	        new AppGameContainer(new WindowGame(), 800, 600, false).start();
+	    }
+
+	    public WindowGame() {
+	        super("Lesson 1 :: WindowGame");
+	    }
+
+	    @Override
+	    public void init(GameContainer container) throws SlickException {
+	        this.container = container;
+	    }
+
+	    public void render(GameContainer container, Graphics g) throws SlickException {
+	    }
+
+	    @Override
+	    public void update(GameContainer container, int delta) throws SlickException {
+	    }
+
+	    @Override
+	    public void keyReleased(int key, char c) {
+	        if (Input.KEY_ESCAPE == key) {
+	            container.exit();
+	        }
+	    }
+}
