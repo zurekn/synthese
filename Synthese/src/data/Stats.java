59598e71c1556257763b6fa84e54cfae2f4a7f52
zurekn <zurek.nicolas@gmail.com>
2015-01-28 15:21:00 +0100

    add stats.java

---
+++ Synthese/src/data/Stats.java
@@ -0,0 +1,93 @@
+package data;
+
+public class Stats {
+	private int life;
+	private int armor;
+	private int mana;
+	private int strength;
+	private int magicPower;
+	private int luck;
+	private int movementPoints;
+	
+	public Stats(int life, int mana) {
+		super();
+		this.life = life;
+		this.mana = mana;
+	}
+
+	public Stats(int life, int armor, int mana, int strength, int magicPower,
+			int luck, int movementPoints) {
+		super();
+		this.life = life;
+		this.armor = armor;
+		this.mana = mana;
+		this.strength = strength;
+		this.magicPower = magicPower;
+		this.luck = luck;
+		this.movementPoints = movementPoints;
+	}
+
+	public int getLife() {
+		return life;
+	}
+
+	public void setLife(int life) {
+		this.life = life;
+	}
+
+	public int getArmor() {
+		return armor;
+	}
+
+	public void setArmor(int armor) {
+		this.armor = armor;
+	}
+
+	public int getMana() {
+		return mana;
+	}
+
+	public void setMana(int mana) {
+		this.mana = mana;
+	}
+
+	public int getStrength() {
+		return strength;
+	}
+
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+
+	public int getMagicPower() {
+		return magicPower;
+	}
+
+	public void setMagicPower(int magicPower) {
+		this.magicPower = magicPower;
+	}
+
+	public int getLuck() {
+		return luck;
+	}
+
+	public void setLuck(int luck) {
+		this.luck = luck;
+	}
+
+	public int getMovementPoints() {
+		return movementPoints;
+	}
+
+	public void setMovementPoints(int movementPoints) {
+		this.movementPoints = movementPoints;
+	}
+
+	@Override
+	public String toString() {
+		return "Stats [life=" + life + ", armor=" + armor + ", mana=" + mana
+				+ ", strength=" + strength + ", magicPower=" + magicPower
+				+ ", luck=" + luck + ", movementPoints=" + movementPoints + "]";
+	}
+	
+}
