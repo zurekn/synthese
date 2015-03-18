01e7396f48f677c6601d39b0b464a3b0fa45408b
frédéric <frdric@10.40.57.163>
2015-03-18 15:06:44 +0100

    images ressource TI

--- Synthese/src/testpackage/ImageProcessingTest.java
+++ Synthese/src/testpackage/ImageProcessingTest.java
@@ -1,9 +1,12 @@
 package testpackage;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 
 import imageprocessing.Pixel;
 import imageprocessing.TraitementImage;
 
+import javax.imageio.ImageIO;
 import javax.swing.SwingUtilities;
 
 
@@ -18,13 +21,36 @@ public static String pathToDir = "Synthese"+File.separator+"res"+File.separator+
 		
 //		SwingUtilities.invokeLater(new WebCamCapture());
 		
-		time = System.currentTimeMillis();
-		
+//		long time = System.currentTimeMillis();
 		TraitementImage ti = new TraitementImage();
+		
+		//////////////////////////////////test//////////////////////////////////
+		String urlImage = "res"+File.separator;
+		BufferedImage img, imgVierge;
+		int seuil = 20;
+		try {
+			img = ImageIO.read(new File(urlImage + "premiere_im_refont.png"));
+			imgVierge = ImageIO.read(new File(urlImage + "premiere_im_refont_vierge.png"));
+			
+			int[][] elementsSubImg = new int[img.getWidth()][img.getHeight()];
+			ti.setImgHeight(img.getHeight());
+		    ti.setImgWidth(img.getWidth());
+		    
+		    elementsSubImg = ti.getSubstractImg(img, imgVierge, seuil);
+		    
+		    ImageIO.write(ti.tableToBufferedImage(elementsSubImg), "png", new File(urlImage + "toto.png"));
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	    ////////////////////////////////////////////////////////////////////////
+		
 //		int seuil = 110;
-		//Pixel p = ti.EtiquetageIntuitifImage(pathToDir+fileName, pathToDir+outFileName);
-		//System.out.println("pixel centre gravité : "+p.getX()+" "+p.getY());
-		System.out.println(System.currentTimeMillis()-time);
+//		Pixel p = ti.EtiquetageIntuitifImage("Manathan.jpg", "Manathan_vide.jpg");
+//		System.out.println("pixel centre gravité : "+p.getX()+" "+p.getY());
+		
+//		List<FormObject> lf = ti.etiquetageIntuitifImage("testBlocPS.jpg", "testBlocBlanc.jpg");
+//		System.out.println(System.currentTimeMillis()-time + " end time");
 	}
 
 }
