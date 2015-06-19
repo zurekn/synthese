805aa79880836ffae88e907398478b733e0f7b4b
zurekn <zurek.nicolas.2.0@gail.com>
2015-06-19 11:58:26 +0200

    debug the update for messages

--- Synthese/src/data/Data.java
+++ Synthese/src/data/Data.java
@@ -39,8 +39,8 @@ public class Data {
 	public static final boolean tiDebug = true;
 	public static final boolean debug = true;
 	public static final boolean DISPLAY_PLAYER = false;
-	public static final boolean runQRCam = true;
-	public static final boolean RUN_APIX = true;
+	public static final boolean runQRCam = false;
+	public static final boolean RUN_APIX = false;
 	public static boolean debugPicture = false; 
 	public static final boolean inTest = true;
 	public static final boolean debugQR = false;
