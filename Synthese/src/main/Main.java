78029850830461c49d2c20076c11f2ccbe2ad892
rulofg <sptg18@gmail.com>
2015-06-22 10:08:41 +0200

    Add ambient music

--- Synthese/src/main/Main.java
+++ Synthese/src/main/Main.java
@@ -23,7 +23,7 @@ public class Main {
 //		Data.SCREEN_WIDTH = 1920;
 //		Data.SCREEN_HEIGHT = 1080;
 		Data.checkValuesIni("paramTI.ini"); // Vérification des variables dans le fichier .ini
-		boolean fullScreen = true;
+		boolean fullScreen = false;
         AppGameContainer gameContaineur =  new AppGameContainer(WindowGame.getInstance(), Data.SCREEN_WIDTH, Data.SCREEN_HEIGHT, fullScreen);
     	gameContaineur.setTargetFrameRate(30);
     	gameContaineur.start();
