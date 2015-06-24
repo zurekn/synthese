f9a9428301dc8cd9aeabe3aad640d70fabff3b95
frédéric <frdric@10.40.57.163>
2015-06-24 14:48:21 +0200

    RAS

--- Synthese/src/data/Data.java
+++ Synthese/src/data/Data.java
@@ -58,12 +58,12 @@ public class Data {
 	public static String IMAGE_DIR = "C:/Users/frédéric/Google Drive/Master Cergy/Projet_PlateauJeu/Synthèse/ImageDeTest/";
 	
 	// Const TI Part
-	public static final long WAIT_TI = 5000;
+	public static long WAIT_TI = 5000;
 	public static int SEUILINITTI = 100;
 	public static int SEUILETI = 200;
 	public static int MIN_SEUIL_FORM = 50;
 	public static int MAX_SEUIL_FORM = 5000;
-	public static final int QRCamSeuil = 60;//Data.SEUILETI;
+	public static int QRCamSeuil = 60;//Data.SEUILETI;
 	
 	public static String NAME = "Jeu de plateau";
 	public static int MAP_WIDTH;
@@ -112,7 +112,7 @@ public class Data {
 
 	public static final int INF = 500;
 
-	public static String MAP_FILE = "Synthese/res/images/map3.tmx";
+	public static String MAP_FILE = "";
 	public static final String MONSTER_DATA_XML = "Synthese/res/xml/monstersData.xml";
 	public static final String SPELLS_DATA_XML = "Synthese/res/xml/spells.xml";
 	public static final String TRAPS_DATA_XML = "Synthese/res/xml/traps.xml";
@@ -143,6 +143,7 @@ public class Data {
 	public static Music BACKGROUND_MUSIC; 
 	
 	//AI
+	public static final float[] AI_BORDERS = {-10.f,-5.f,-2.f};
 	public static final int AI_DEPTH_MAX = 1;
 	public static final float[][] AI_FACTORS = {
 			{ 2.f, 1.5f, 0.75f, 0.2f, 0.50f, 0.3f }, // coward
@@ -166,6 +167,7 @@ public class Data {
 	public static final int LIFE = 1 ;
 	public static final int ALLIES_LIFE = 2;
 	public static final int MANA = 3;
+	public static final int TARGET_DETECTION = 4;
 
 	public static final int ENEMIES_DETECTION = 0; 
 	public static final int ALLIES_DETECTION = 1;
@@ -199,6 +201,8 @@ public class Data {
 	public static String MAIN_TEXT = "";
 	public static int MESSAGE_MAX_LENGTH;
 
+	public static Image WIN_IMAGE;
+	public static Image LOSE_IMAGE;
 	
 	/**
 	 * Load all game variables
@@ -276,6 +280,8 @@ public class Data {
 			
 			BACKGROUND_MUSIC = new Music(root.getChildText("music"));
 			IMAGE_HALO = new Image(root.getChildText("halo_image"));
+			WIN_IMAGE = new Image(root.getChildText("win_image"));
+			LOSE_IMAGE = new Image(root.getChildText("lose_image"));
 		} catch (DataConversionException e) {
 			e.printStackTrace();
 		} catch (SlickException e) {
@@ -317,50 +323,97 @@ public class Data {
 		Scanner scanner;
 		try {
 			File file = new File(filePath);
-			if(file.exists())
+			if(file.exists() && file.length() > 0)
 			{
+				System.out.println("length = "+file.length());
 				scanner = new Scanner(file);	
 				while (scanner.hasNextLine()) 
 				{
 				    String line = scanner.nextLine();
-				    if(line.contains("="))
+				    if(line.contains("=") && !line.contains("#"))
 				    {
-				    	line = line.replaceAll(" ", "");
-				    	if(line.contains("seuilinit"))
-				    		if(!line.substring(line.lastIndexOf("=")+1).equals(""))
-				    			Data.SEUILINITTI = Integer.parseInt(line.substring(line.lastIndexOf("=")+1));
-				    	if(line.contains("seuiletiquetage"))
-				    		if(!line.substring(line.lastIndexOf("=")+1).equals(""))
-				    			Data.SEUILETI = Integer.parseInt(line.substring(line.lastIndexOf("=")+1));
-				    	if(line.contains("seuilmin"))
-				    		if(!line.substring(line.lastIndexOf("=")+1).equals(""))
-				    			Data.MIN_SEUIL_FORM = Integer.parseInt(line.substring(line.lastIndexOf("=")+1));
-				    	if(line.contains("seuilmax"))
-				    		if(!line.substring(line.lastIndexOf("=")+1).equals(""))
-				    			Data.MAX_SEUIL_FORM = Integer.parseInt(line.substring(line.lastIndexOf("=")+1));
+				    	try {
+					    	line = line.replaceAll(" ", "");
+					    	line = line.replaceAll(";", "");
+					    	if(line.contains("seuilinit"))
+					    		if(!line.substring(line.lastIndexOf("=")+1).equals(""))
+					    			Data.SEUILINITTI = Integer.parseInt(line.substring(line.lastIndexOf("=")+1));
+					    	if(line.contains("seuiletiquetage"))
+					    		if(!line.substring(line.lastIndexOf("=")+1).equals(""))
+					    			Data.SEUILETI = Integer.parseInt(line.substring(line.lastIndexOf("=")+1));
+					    	if(line.contains("seuilmin"))
+					    		if(!line.substring(line.lastIndexOf("=")+1).equals(""))
+					    			Data.MIN_SEUIL_FORM = Integer.parseInt(line.substring(line.lastIndexOf("=")+1));
+					    	if(line.contains("seuilmax"))
+					    		if(!line.substring(line.lastIndexOf("=")+1).equals(""))
+					    			Data.MAX_SEUIL_FORM = Integer.parseInt(line.substring(line.lastIndexOf("=")+1));
+					    	if(line.contains("qrcamseuil"))
+					    		if(!line.substring(line.lastIndexOf("=")+1).equals(""))
+					    			Data.QRCamSeuil = Integer.parseInt(line.substring(line.lastIndexOf("=")+1));
+					    	if(line.contains("initmaxtime"))
+					    		if(!line.substring(line.lastIndexOf("=")+1).equals(""))
+					    			Data.INIT_MAX_TIME = Integer.parseInt(line.substring(line.lastIndexOf("=")+1));
+					    	if(line.contains("musicvolum"))
+					    		if(!line.substring(line.lastIndexOf("=")+1).equals(""))
+					    			Data.MUSIC_VOLUM = Float.parseFloat(line.substring(line.lastIndexOf("=")+1));
+					    	if(line.contains("gamewait"))
+					    		if(!line.substring(line.lastIndexOf("=")+1).equals(""))
+					    		{	
+					    			int tempWait = Integer.parseInt(line.substring(line.lastIndexOf("=")+1));
+					    			if(tempWait > 2000 && tempWait < 10000)
+					    				Data.WAIT_TI  = tempWait;
+					    		}
+				    	} 
+				    	catch (Exception e) 
+				    	{e.printStackTrace();} 
 				    }
 				}
 				scanner.close();
 			}
 			else // file does not exist
 			{
+				System.out.println("fail------------------------------");
 				try {
 					file.createNewFile();
 					FileWriter writer = new FileWriter(file, true);
 
-					String texte = "seuilinit="+Data.SEUILINITTI+"\n";
+					String texte = "seuilinit="+Data.SEUILINITTI+";";
 					writer.write(texte,0,texte.length());
 					writer.write("\r\n");
 					
-					texte = "seuiletiquetage="+Data.SEUILETI;
+					texte = "seuiletiquetage="+Data.SEUILETI+";";
 					writer.write(texte,0,texte.length());
 					writer.write("\r\n");
 					
-					texte = "seuilmin="+Data.MIN_SEUIL_FORM;
+					texte = "seuilmin="+Data.MIN_SEUIL_FORM+";";
 					writer.write(texte,0,texte.length());
 					writer.write("\r\n");
 					
-					texte = "seuilmax="+Data.MAX_SEUIL_FORM;
+					texte = "seuilmax="+Data.MAX_SEUIL_FORM+";";
+					writer.write(texte,0,texte.length());
+					writer.write("\r\n");
+					
+					texte = "";
+					writer.write(texte,0,texte.length());
+					writer.write("\r\n");
+					
+					texte = "qrcamseuil="+Data.QRCamSeuil+";";
+					writer.write(texte,0,texte.length());
+					writer.write("\r\n");
+					
+					texte = "initmaxtime="+Data.INIT_MAX_TIME+";";
+					writer.write(texte,0,texte.length());
+					writer.write("\r\n");
+					
+					texte = "musicvolum="+Data.MUSIC_VOLUM+";";
+					writer.write(texte,0,texte.length());
+					writer.write("\r\n");
+					
+					texte = "#Attention, ce paramètre a un fort impact sur le jeu";
+					writer.write(texte,0,texte.length());
+					writer.write("\r\n");
+					
+					texte = "gamewait="+Data.WAIT_TI+";";
 					writer.write(texte,0,texte.length());
 					writer.write("\r\n");
 					
